efe077bdc27c0e07bb117a2b313a96feb097e709
ByNatO <mine19n@gmail.com>
2020-04-01 15:07:57 +0000

    link address changed

--- app.py
+++ app.py
@@ -886,6 +886,4 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
 
 if __name__ == '__main__':
    
-
-    app.run_server(port=8080,
-                  host='127.0.0.1')
+    app.run_server(debug=False)
