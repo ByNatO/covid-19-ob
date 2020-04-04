ec6430b509588ffd1e167616974f651ab1318037
ByNatO <mine19n@gmail.com>
2020-04-04 05:52:06 +0000

    indicating the date of last update

--- app.py
+++ app.py
@@ -252,6 +252,7 @@ def etl(source='web'):
 # data = etl(source='folder')
 data = pd.read_csv('dashboard_data.csv')
 data['date'] = pd.to_datetime(data['date'])
+update = data['date'].dt.strftime('%B %d, %Y').max()
 
 geo_us = pd.read_csv('geo_us.csv')
 
@@ -744,6 +745,11 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
             'color': colors['text']
             }
         ),
+    
+    html.Div(children='Data last updated {} '.format(update), style={
+        'textAlign': 'center',
+        'color': colors['text']
+        }),
 
     html.Div(children='Select focus for the dashboard', style={
         'textAlign': 'center',
